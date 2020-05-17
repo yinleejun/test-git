af50901338a97d40aa0ba47e04d648d55e02c3a7
yinleejun <39074325+yinleejun@users.noreply.github.com>
2020-05-17 08:43:40 +0800

    Update Test.java

--- git-demo/src/com/ylj/Test.java
+++ git-demo/src/com/ylj/Test.java
@@ -4,31 +4,9 @@ import java.util.Date;
 
 public class Test {
     private String name;
-    private int age;
-    private String address;
-    private String school_name;
-    private Date date;
-
-    private String level;
-//    private String sex;
-
-//    private String level;
-
-    public String mid;
-
-    public String becifNo;
-
-    public String power;
-    public String lab;
-    
 
     public void getName(){
         System.out.println("name =" + "lisi");
-        System.out.println("name =" + "zhangsan");
-        System.out.println("mast-idea");
-        System.out.println("mast-github");
-        System.out.println("name =" + "wangwu");
     }
-
 
 }
